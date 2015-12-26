615406eabffbec40c8b811c10bd2467a194f3424
Dev-I-Ant <xavlec@gmail.com>
2015-12-26 20:34:59 +0100

    Make some typescript work on both client and server
    Typescript generation :
    - System on client
    - CommonJS on server
    No tsc command (until some tsc allowing several tsconfig)

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+"use strict";
 var express = require('express');
 var path = require('path');
 var app = express();
@@ -8,5 +9,4 @@ var server = app.listen(3000, function () {
     var port = server.address().port;
     console.log('App listening at http://%s:%s', host, port);
 });
-module.exports = app;
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,11 +1,12 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+import * as http from 'http';
+import * as express from 'express';
+import * as path from 'path';
+
+var app: express.Express = express();
 // view engine setup
 app.use(express.static(path.join(__dirname, 'public')));
-var server = app.listen(3000, function() {
+var server: http.Server = app.listen(3000, function() {
   var host = 'localhost';
   var port = server.address().port;
   console.log('App listening at http://%s:%s', host, port);
 });
-module.exports = app;
